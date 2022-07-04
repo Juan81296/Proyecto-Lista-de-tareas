7be81bee66d921996bdc18ea5e32b53a25b68b3d
Juan Tenaglia <juantenaglia81296@gmail.com>
2022-07-04 17:57:51 -0300

    Lista-de-tareas

--- src/componentes/Tarea.js
+++ src/componentes/Tarea.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import '../hojas-de-estilo/Tarea.css'
-import { AiOutlineCloseCircle } from "react-icons/ai";
 
 function Tarea({ id,texto,completada,completarTarea,eliminarTarea }){
   return(
@@ -11,7 +10,7 @@ function Tarea({ id,texto,completada,completarTarea,eliminarTarea }){
       </div>
       <div className='tarea-contenedor-iconos'
       onClick={()=>eliminarTarea(id)}>
-        <AiOutlineCloseCircle className='tarea-icono'/>
+        <div className='tarea-icono'>X</div>
       </div>
     </div>
   );
